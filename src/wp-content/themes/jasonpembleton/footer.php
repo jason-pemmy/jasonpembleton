64fda542802b8b04b540e59e8e36be2a8300d62a
Jason <jason.pemmy@gmail.com>
2016-11-21 22:14:40 -0500

    werk

--- src/wp-content/themes/jasonpembleton/footer.php
+++ src/wp-content/themes/jasonpembleton/footer.php
@@ -6,7 +6,7 @@
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-4">
+					<div class="col-sm-6">   
 						<div class="footer-image-container">
 							<img src="<?php echo get_template_directory_uri() ?>/images/footer-image.png" />
 						</div>
@@ -20,7 +20,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-sm-4">
+					<div class="col-sm-6">
 						<div class="footer-main-menu">
 							<?php
 								wp_nav_menu( array(
