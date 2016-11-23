c26bd343c8ca58a9fdaaf0ae0dffb8800de72ec8
Jason <jason.pemmy@gmail.com>
2016-11-22 21:31:07 -0500

    werk

--- src/wp-content/themes/jasonpembleton/views/shortcodes/about-section.php
+++ src/wp-content/themes/jasonpembleton/views/shortcodes/about-section.php
@@ -1,4 +1,10 @@
-<div class="about-section-container" style="background-image:url('<?php echo $image;?>')">	
-	<div class="about-heading"><?php echo $heading; ?></div>
-	<div class="about-copy"><?php echo $copy; ?></div>
-</div>  
+<div class="about-section-container" style="background-image:url('<?php echo $image;?>')">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="about-heading"><?php echo $heading; ?></div>
+                <div class="about-copy"><?php echo $copy; ?></div>    
+            </div>
+        </div>        
+    </div>	
+</div>   
